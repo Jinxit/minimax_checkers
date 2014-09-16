346cdeb76d1c925cee4f661f9a71c29fbc013ecf
Lucas Åström <lucasas@kth.se>
2014-09-16 22:07:38 +0200

    changed to alphabeta algorithm

--- src/player.cpp
+++ src/player.cpp
@@ -35,8 +35,8 @@ namespace checkers
 	 	int maxIndex = -1;
 	 	for (uint i = 0; i < lNextStates.size(); i++)
 	 	{
-	 		int depth = 20;
-	 		int score = minimax(lNextStates[i], depth, true, pDue);
+	 		int depth = 10;
+	 		int score = alphabeta(lNextStates[i], depth, INT_MIN, INT_MAX, true, pDue);
 	 		
 	 		if (maxScore < score)
 	 		{
@@ -48,7 +48,7 @@ namespace checkers
 	    return lNextStates[maxIndex];
 	}
 
-	int Player::minimax(const GameState &node, int depth, bool maximizingPlayer, const Deadline &pDue)
+	int Player::alphabeta(const GameState &node, int depth, int alpha, int beta, bool maximizingPlayer, const Deadline &pDue)
 	{
 		std::string nodeString = node.toMessage();
 		if (scoreMap.count(nodeString) > 0)
@@ -60,37 +60,53 @@ namespace checkers
 			return getScore(node);
 		}
 		
-		int bestValue = INT_MAX;
-		if (maximizingPlayer)
-		{
-			bestValue = INT_MIN;
-		}
 		if (pDue < Deadline::now())
 		{
-			return bestValue + (maximizingPlayer ? 1 : -1);
+			if (maximizingPlayer)
+			{
+				return INT_MIN + 1;
+			}
+			else
+			{
+				return INT_MAX - 1;
+			}
 		}
 
 		std::vector<GameState> children;
 	    node.findPossibleMoves(children);
 
-	    for (uint i = 0; i < children.size(); i++)
-	    {
-	    	int val = minimax(children[i], depth - 1, !maximizingPlayer, pDue);
-
-	    	if (maximizingPlayer)
-	    	{
-	    		bestValue = max(bestValue, val);
+    	if (maximizingPlayer)
+    	{
+		    for (uint i = 0; i < children.size(); i++)
+		    {
+	    		alpha = max(alpha, alphabeta(children[i], depth - 1, alpha, beta, !maximizingPlayer, pDue));
+	    		if (beta <= alpha)
+	    		{
+	    			break;
+	    		}
 	    	}
-	    	else
-	    	{
-	    		bestValue = min(bestValue, val);
-	    	}
-	    }
 
-	    scoreMap[nodeString] = bestValue;
-	    // also add the reverse
-	    scoreMap[node.reversed().toMessage()] = -bestValue;
-	    return bestValue;
+		    scoreMap[nodeString] = alpha;
+		    // also add the reverse
+		    scoreMap[node.reversed().toMessage()] = -alpha;
+	    	return alpha;
+    	}
+    	else
+    	{
+		    for (uint i = 0; i < children.size(); i++)
+		    {
+	    		beta = min(beta, alphabeta(children[i], depth - 1, alpha, beta, !maximizingPlayer, pDue));
+	    		if (beta <= alpha)
+	    		{
+	    			break;
+	    		}
+    		}
+
+		    scoreMap[nodeString] = beta;
+		    // also add the reverse
+		    scoreMap[node.reversed().toMessage()] = -beta;
+    		return beta;
+    	}
 	}
 
 	int Player::getScore(const GameState &node)
